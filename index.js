136b64d39c6558bcd290b6f724a58a24935b66c7
jklinck <joeklinck@hotmail.com>
2016-12-03 14:56:03 -0800

    no longer fetching laptop-min.jpg externally

--- index.js
+++ index.js
@@ -1,12 +1,44 @@
 $(document).ready(function(){
-	$(document).on('click','.navbar-collapse.in',function(e) {
-	    if( $(e.target).is('a') && $(e.target).attr('class') != 'dropdown-toggle'){
-	        $(this).collapse('hide');
-	    };
-    });
-    var date = new Date();
-    $("#year").html(date.getFullYear());
+	// $('.navbar-collapse').on('click',function(e){
+	//     if($(e.target).is('a')){
+	//         $(this).collapse('hide');
+	//     };
+ //    });
+    // var date = new Date();
+    // $("#year").html(date.getFullYear());
 });
+
+// var drop=document.querySelector(".drpDn");
+// drop.addEventListener("click",function(){
+
+// })
+
+var iconBar=document.querySelector(".icon-bar");
+var dropDown=false;
+var about = document.querySelector(".drpDn");
+iconBar.addEventListener("click",function(){
+	if(dropDown===false){
+		about.display="inline-block";
+		dropDown=true;
+	}
+	else{
+		about.display="none";
+		dropDown=false;
+	}
+});
+
+var navBar = document.getElementById("navBar");
+about.addEventListener("click",function(){
+	about.display="none";
+})
+
+
+var date = new Date();
+var newYear = date.getFullYear();
+document.getElementById("year").innerHTML=newYear;
+
+
+
 
 
 
