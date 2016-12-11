f22329340e2e9c0b6a13c6fd34c3f953e951cacf
jklinck <joeklinck@hotmail.com>
2016-12-10 23:01:42 -0800

    fixed responsive dropdown

--- index.js
+++ index.js
@@ -1,48 +1,10 @@
 $(document).ready(function(){
-	// $('.navbar-collapse').on('click',function(e){
-	//     if($(e.target).is('a')){
-	//         $(this).collapse('hide');
-	//     };
- //    });
-    // var date = new Date();
-    // $("#year").html(date.getFullYear());
+	$('.navbar-collapse').on('click',function(e){
+	    if($(e.target).is('a')){
+	        $(this).collapse('hide');
+	    };
+    });
+    var date = new Date();
+    $("#year").html(date.getFullYear());
 });
-
-// var drop=document.querySelector(".drpDn");
-// drop.addEventListener("click",function(){
-
-// })
-
-var iconBar=document.querySelector(".icon-bar");
-var dropDown=false;
-var about = document.querySelector(".drpDn");
-iconBar.addEventListener("click",function(){
-	if(dropDown===false){
-		about.display="inline-block";
-		dropDown=true;
-	}
-	else{
-		about.display="none";
-		dropDown=false;
-	}
-});
-
-var navBar = document.getElementById("navBar");
-about.addEventListener("click",function(){
-	about.display="none";
-})
-
-
-var date = new Date();
-var newYear = date.getFullYear();
-document.getElementById("year").innerHTML=newYear;
-
-
-
-
-
-
-
-
-
 
